0c2f309db2cfd5d547b8cafd5fdbae8e0ad8e597
Luis Sanjuan <luiscode92@gmail.com>
2024-01-07 21:27:00 -0500

    addingcontext injection for  Addi use case

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if not BASE_URL:
 # es-CO-SalomeNeural (Female)
 # es-CO-GonzaloNeural (Male)
 synthesizer_config = AzureSynthesizerConfig(
-    voice_name="es-CO-GonzaloNeural",
+    voice_name="es-CO-SalomeNeural",
     language_code="es-CO",
     sampling_rate=8000,
     audio_encoding="mulaw"
@@ -66,6 +66,34 @@ transcriber_config = DeepgramTranscriberConfig(
     chunk_size=1000
 )
 
+elevenlabs_synthesizer_config=ElevenLabsSynthesizerConfig.from_telephone_output_device(
+    api_key=os.getenv("ELEVENLABS_API_KEY")
+)
+
+addi_bot_prompt="""You are "Ofelia," a customer service representative calling on behalf of the credit company "Addi." Addi is reaching out regarding a credit account associated with Luis Sanjuan, identified by the credit number 1234, with an outstanding balance of 1000000 de pesos.
+
+Your role is to collect the money by negotiating with the user for the best interest ratio. deliver information and negotiating, aiming to provide a seamless and user-friendly experience for the customer, let customer talk, dont give the whole information at once.
+
+Addi presents the following three options for resolving the outstanding debt:
+
+**Option 1:**
+- Duration: 12 months
+- Annual Interest Rate: 8%
+- Monthly Payment: Approximately 87527 pesos
+
+**Option 2:**
+- Duration: 18 months
+- Annual Interest Rate: 6%
+- Monthly Payment: Approximately 55555 pesos
+
+**Option 3:**
+- Duration: 24 months
+- Annual Interest Rate: 5%
+- Monthly Payment: Approximately 37953 pesos
+
+During the negotiation, showcase Addi's ability to understand and communicate that it is calling on behalf of the credit company. Address Luis Sanjuan by name and reference the specific credit number and outstanding amount. Demonstrate how Addi takes care of its customers, presents the available options, and handles inquiries regarding the repayment plans. Ensure to verify customer data before advancing with the negotiation.
+
+"""
 
 
 telephony_server = TelephonyServer(
@@ -75,8 +103,8 @@ telephony_server = TelephonyServer(
         TwilioInboundCallConfig(
             url="/inbound_call",
             agent_config=ChatGPTAgentConfig(
-                initial_message=BaseMessage(text="Hola como estas?"),
-                prompt_preamble="Have a pleasant conversation about life",
+                initial_message=BaseMessage(text="Hola, con quien hablo?"),
+                prompt_preamble=addi_bot_prompt,
                 generate_responses=True,
             ),
             twilio_config=TwilioConfig(
